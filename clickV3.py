bba59c2729b277ca97daa5d01e2785d4b650edc0
Riad Hak <99054420@mydavinci.nl>
2022-03-10 21:24:53 +0100

    opdKlaar

--- clickV3.py
+++ clickV3.py
@@ -1,58 +1,12 @@
-# import tkinter as tk
-
-
-# def up():
-#     global number
-#     number += 1
-#     clicker()
-# def down():
-#     global number
-#     number -= 1
-#     clicker()
-
-# def clicker():
-#     global number,label1
-#     if number < 0:
-#         window.config(bg='red')
-#     elif number > 0 :
-#         window.config(bg='green')
-#     elif number == 0:
-#         window.config(bg='grey')
-#     labstr = tk.StringVar(value=f'{number}')
-#     label1.config(textvariable=labstr)
-# def click(event):
-#     global number
-#     print('clicked')
-#     if up :
-#         number = number * 3
-#     elif down :
-#         number = number / 3
-
-
-# window = tk.Tk()
-# window.geometry('300x300')
-
-# number = 0 
-# label1 = tk.Label( text= f'{number}',width=20)
-# label1.place(anchor='center',relx=0.5,rely = 0.5)
-# button1 = tk.Button(text='Up')
-# button2 = tk.Button(text='Down')
-# button1.pack(side=tk.TOP)
-# button2.pack(side=tk.BOTTOM)
-# window.bind("<Double-Button-1>",click)
-
-# clicker()
-# window.mainloop()
-
 import tkinter as tk 
 #=================== funtions hier onder=================================
-def up():
+def up(higher):
     global number,keer
     number += 1
     print(number)
     keer = True
     clicker()
-def down():
+def down(lower):
     global number, keer
     number -= 1
     print(number)
@@ -81,19 +35,23 @@ def Dclick(event):
 #==================== window/labels/buttons setup hieronder====================
 keer = False
 number = 0
+
 win = tk.Tk()
 win.geometry('300x150')
+
 lab = tk.Label( text= f'{number:.0f}',width=20)
 lab.config(bg='white')
+lab.place(anchor='center',relx=0.5,rely = 0.5)
+
 button1 = tk.Button(text='Up',width=20)
 button2 = tk.Button(text='Down',width=20)
-lab.place(anchor='center',relx=0.5,rely = 0.5)
 button1.pack(side=tk.TOP)
 button2.pack(side=tk.BOTTOM)
 button1.config(command=up)
 button2.config(command=down)
-button1.bind("+",up)
-button2.bind("-",down)
+
+win.bind("+",up)
+win.bind("-",down)
 lab.bind("<Double-Button-1>",Dclick)
 clicker()
 
